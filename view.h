9317c907d57ccb3aab0de65faf799f37dc0601c6
AlessioTommasi <126590912+AlessioTommasi-polimi@users.noreply.github.com>
2023-07-12 10:27:03 +0200

    fix stampe

--- view.h
+++ view.h
@@ -1,24 +1,25 @@
 void printparcoVeicoli(parco_veicoli *parco)
 {
-    printf("{\n");
-    printf("  \"Size\": %d,\n", parco->actual_size);
-    printf("  \"Capacity\": %d,\n", parco->actual_capacity);
+    printf("           Parco Veicoli:{\n");
+    printf("            \"Size\": %d,\n", parco->actual_size);
+    printf("            \"Capacity\": %d,\n", parco->actual_capacity);
+    printf("             Veicoli:[\n");
 
-    printf("    Parco Veicoli:{\n     [\n");
+    
     for (int i = 0; i < parco->actual_size; i++)
     {
-        printf("        {\n");
-        printf("          \"numero veicolo\": %d,\n", i + 1);
+        printf("                 {\n");
+        printf("                   \"numero veicolo\": %d,\n", i + 1);
         printVeicolo(&parco->veicoli[i]);
-        printf("        }%s\n", (i < parco->actual_size - 1) ? "," : "");   
+        printf("                 }%s\n", (i < parco->actual_size - 1) ? "," : "");
     }
-    printf("     ]\n");
-    printf("    }\n");
+    printf("              ]\n");
+    printf("             }\n");
 }
 
 
 void printVeicolo (veicolo *v){
-    printf("          \"Autonomia\": %d,\n", v->autonomia);
+    printf("                   \"Autonomia\": %d,\n", v->autonomia);
     printf("\n");
 }
 
@@ -40,8 +41,8 @@ void printHighway(){
     {
         printf("        {\n");
         printf("          \"Stazione\": %d,\n", i + 1);
-        printf("         \"Distanza\": %d,\n", highway.stazioni[i].distanza_da_inizio_autostada);
-        printf("         \"Index\": %d,\n", highway.stazioni[i].index);
+        printf("          \"Distanza\": %d,\n", highway.stazioni[i].distanza_da_inizio_autostada);
+        printf("          \"Index\": %d,\n", highway.stazioni[i].index);
         printparcoVeicoli(&highway.stazioni[i].parco);
         printf("   }%s\n", (i < highway.actual_size - 1) ? "," : "");   
     }
